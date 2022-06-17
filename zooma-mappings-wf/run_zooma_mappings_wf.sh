1bfb4f6cc0ddbf589e1579a06d62496469017ada
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2022-06-17 09:15:51 +0100

    add get_working_dir function

--- zooma-mappings-wf/run_zooma_mappings_wf.sh
+++ zooma-mappings-wf/run_zooma_mappings_wf.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+MODE="atlas"
 FORCEALL=${FORCEALL:-false}
 RESTART_TIMES=${RESTART_TIMES:-3}
 NJOBS=${NJOBS:-2}
@@ -11,11 +12,15 @@ ZOOMA_META_URL=${ZOOMA_API_BASE}/server/metadata
 [ -z ${SN_CONDA_PREFIX +x} ] && echo "Env var SN_CONDA_PREFIX needs to be defined." && exit 1
 
 # -conda-prefix $SN_CONDA_PREFIX
-snakemake --use-conda --conda-frontend mamba --restart-times --config \
+snakemake --use-conda --conda-frontend mamba --restart-times $RESTART_TIMES --config \
     mode=$MODE \
     zooma_exclusions=$ZOOMA_EXCLUSIONS \
-    zoomaMetadataUrl=$ZOOMA_META_URL
+    zoomaMetadataUrl=$ZOOMA_META_URL \
     notifEmail=$EMAIL \
     retryWithoutZooma=$RETRYWOUTZOOMA \
+    atlas_exps=$ATLAS_EXPS \
+    atlas_sc_experiments=$ATLAS_SC_EXPERIMENTS \
+    irap_single_lib=$IRAP_SINGLE_LIB/zoomage \
     -j $NJOBS -s Snakefile
+
 
