61d00f7c94f6b08dde14cf53a6934c2d3838b9e6
Iris Diana Yu <irisyu@ebi.ac.uk>
2022-06-17 19:27:25 +0800

    Add conditionals to determine working dir

--- zooma-mappings-wf/run_zooma_mappings_wf.sh
+++ zooma-mappings-wf/run_zooma_mappings_wf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MODE="atlas"
+MODE=${mode:-"atlas"}
 FORCEALL=${FORCEALL:-false}
 RESTART_TIMES=${RESTART_TIMES:-3}
 NJOBS=${NJOBS:-2}
@@ -12,7 +12,19 @@ ZOOMA_META_URL=${ZOOMA_API_BASE}/server/metadata
 [ -z ${SN_CONDA_PREFIX +x} ] && echo "Env var SN_CONDA_PREFIX needs to be defined." && exit 1
 
 
-WORKING_DIR=
+# Determine working directory to use
+if [ "$MODE" == "atlas" ]; then
+  WORKING_DIR=$ATLAS_EXPS
+elif [ "$MODE" == "single_cell" ]; then
+  [ -z ${ATLAS_SC_EXPERIMENTS+x} ] && echo "Env var ATLAS_SC_EXPERIMENTS pointing to the directory for Atlas SC Experiments needs to be defined to run with SC data." && exit 1
+  WORKING_DIR=$ATLAS_SC_EXPERIMENTS
+elif [ "$MODE" == "irap_single_lib" ]; then
+  WORKING_DIR=$IRAP_SINGLE_LIB/zoomage
+else
+  echo "ERROR: mode: $MODE not recognised"
+  exit 1
+fi
+
 
 # -conda-prefix $SN_CONDA_PREFIX
 snakemake --use-conda --conda-frontend mamba --restart-times $RESTART_TIMES --config \
