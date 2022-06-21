8a1191b0411206bc0eec8649a7ff347a9aad201b
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2022-06-21 15:43:16 +0100

    finalise rules of main workflow

--- zooma-mappings-wf/run_zooma_mappings_wf.sh
+++ zooma-mappings-wf/run_zooma_mappings_wf.sh
@@ -1,45 +1,37 @@
 #!/usr/bin/env bash
 
 MODE=${MODE:-"atlas"}
-ATLAS_PROD_BRANCH=${ATLAS_PROD_BRANCH:-"develop"}
+ZOOMA_EXCLUSIONS=$( pwd )/zooma_exclusions.yml
 FORCEALL=${FORCEALL:-true} # set to true. If the file is present it won't update
+if [ "$FORCEALL" = true ]; then FORCE_ALL="--forceall"; else FORCE_ALL=""; fi
+
 RESTART_TIMES=${RESTART_TIMES:-3}
 NJOBS=${NJOBS:-2}
 EMAIL=${EMAIL:-false}
 RETRYWOUTZOOMA=${RETRYWOUTZOOMA:-yes} # or true
 ZOOMA_META_URL=${ZOOMA_API_BASE}/server/metadata
-# temp dir for zooma mapping reports
-TEMP_DIR=/hps/nobackup/ma/... #$FG_ATLAS_TMP !!!!!!!!!!!!!!
-ZOOMA_EXCLUSIONS=
 
-# Check that relevant env vars are set
-[ -z ${SN_CONDA_PREFIX +x} ] && echo "Env var SN_CONDA_PREFIX needs to be defined." && exit 1
-
-
-# Determine working directory to use
-if [ "$MODE" == "atlas" ]; then
-  WORKING_DIR=$ATLAS_EXPS
-elif [ "$MODE" == "single_cell" ]; then
-  [ -z ${ATLAS_SC_EXPERIMENTS+x} ] && echo "Env var ATLAS_SC_EXPERIMENTS pointing to the directory for Atlas SC Experiments needs to be defined to run with SC data." && exit 1
-  WORKING_DIR=$ATLAS_SC_EXPERIMENTS
-elif [ "$MODE" == "irap_single_lib" ]; then
-  WORKING_DIR=$IRAP_SINGLE_LIB/zoomage
-else
-  echo "ERROR: mode: $MODE not recognised"
-  exit 1
-fi
-
-
-# Derive variables and source script for apply_fixes rule
+ATLAS_PROD_BRANCH=${ATLAS_PROD_BRANCH:-"develop"}
 PROCESSED_BRANCH=$(echo $ATLAS_PROD_BRANCH | sed 's+/+_+g')
 ATLAS_PROD_CO="${ATLAS_PROD}/sw/atlasinstall_branches/atlasprod_${PROCESSED_BRANCH}/atlasprod"
-#EXP_METADATA_DIR="${ATLAS_PROD_CO}/experiment_metadata"
-#source ${ATLAS_PROD_CO}/bash_util/generic_routines.sh
-export -f applyAllFixesForExperiment
+LSF_CONFIG=$( pwd )/lsf.yaml
+
+# Check that relevant env vars are set
+[ -z ${WORKING_DIR +x} ] && echo "Env var WORKING_DIR needs to be defined." && exit 1
+
+[ -z ${TEMP_DIR +x} ] && echo "Env var TEMP_DIR needs to be defined." && exit 1
+
+[ -z ${CONDA_PREFIX_LINE +x} ] && echo "Env var CONDA_PREFIX_LINE needs to be defined." && exit 1
+
+[ -z ${ZOOMA_META_URL +x} ] && echo "Env var ZOOMA_META_URL needs to be defined." && exit 1
+
+[ -z ${EXPERIMENT_METADATA_DIR +x} ] && echo "Env var EXPERIMENT_METADATA_DIR needs to be defined." && exit 1
 
 
 # -conda-prefix $SN_CONDA_PREFIX
-snakemake --use-conda --conda-frontend mamba --restart-times $RESTART_TIMES --config \
+snakemake --dry-run --use-conda --conda-frontend mamba --restart-times $RESTART_TIMES \
+    --latency-wait 90 --keep-going \
+    $PROFILE_LINE $CONDA_PREFIX_LINE $FORCE_ALL  --config \
     mode=$MODE \
     zooma_exclusions=$ZOOMA_EXCLUSIONS \
     temp_dir=$TEMP_DIR \
@@ -47,8 +39,9 @@ snakemake --use-conda --conda-frontend mamba --restart-times $RESTART_TIMES --co
     notifEmail=$EMAIL \
     retryWithoutZooma=$RETRYWOUTZOOMA \
     working_dir=$WORKING_DIR \
+    experiment_metadata_dir=$EXPERIMENT_METADATA_DIR \
     atlas_prod_co=$ATLAS_PROD_CO \
-    #exp_metadata_dir=$EXP_METADATA_DIR \
+    lsf_config=$LSF_CONFIG \
     -j $NJOBS -s Snakefile
 
 
